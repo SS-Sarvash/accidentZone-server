db8bf781dd22f561178d8737e5594158ceaa0e70
SS-Sarvash <sarvashsaravanan.com>
2023-10-31 18:30:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const dotenv = require('dotenv');
+dotenv.config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT;
+
+app.get("/getdata", async (req, res) => {
+    try {
+      const qry = await pool.query("SELECT * FROM accidentZone;");
+      res.json(qry.rows);
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+
+app.post("/postdata", async (req, res) => {
+    try {
+      const { Status, Latitude, Longitude } = req.body;
+      const qry = await pool.query(
+        `INSERT INTO main_data(Status, Latitude, Longitude) values($1,$2,$3)`,
+        [Status, Latitude, Longitude]
+      );
+      res.json(qry);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+app.listen(port, () => {
+    console.log(`Server is running on port${port}`);
+  });
