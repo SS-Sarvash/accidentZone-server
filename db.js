a06e69885de95c13a52be68db7ae40036ef15337
SS-Sarvash <sarvashsaravanan.com>
2023-10-31 19:00:22 +0530

    fault in pool string rectified

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 const dotenv = require('dotenv');
 dotenv.config()
 const Pool = require("pg").Pool;
-const pool = new Pool({
-  user: "postgres",
-  password: "12345",
-  database: "test1",
-  port: 5432,
-  host: "localhost",
-});
-
 // const pool = new Pool({
-//   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-// })
+//   user: "postgres",
+//   password: "12345",
+//   database: "test1",
+//   port: 5432,
+//   host: "localhost",
+// });
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
 
 pool.connect((err)=>{
   if(err) throw err;
